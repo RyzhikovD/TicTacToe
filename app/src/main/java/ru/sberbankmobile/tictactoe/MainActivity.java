855c1dbe8ce4f3feb8a7d22e1ed32555d21ee80d
dmitriy <ryzhikov.dmvl@gmail.com>
2019-10-24 00:01:24 +0300

    Draw case added

--- app/src/main/java/ru/sberbankmobile/tictactoe/MainActivity.java
+++ app/src/main/java/ru/sberbankmobile/tictactoe/MainActivity.java
@@ -1,26 +1,16 @@
 package ru.sberbankmobile.tictactoe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ImageButton mButton1;
-    private ImageButton mButton2;
-    private ImageButton mButton3;
-    private ImageButton mButton4;
-    private ImageButton mButton5;
-    private ImageButton mButton6;
-    private ImageButton mButton7;
-    private ImageButton mButton8;
-    private ImageButton mButton9;
 
     private List<ImageButton> mButtons = new ArrayList<>(9);
 
@@ -36,16 +26,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mButton1 = findViewById(R.id.button_1);
-//        mButton2 = findViewById(R.id.button_2);
-//        mButton3 = findViewById(R.id.button_3);
-//        mButton4 = findViewById(R.id.button_4);
-//        mButton5 = findViewById(R.id.button_5);
-//        mButton6 = findViewById(R.id.button_6);
-//        mButton7 = findViewById(R.id.button_7);
-//        mButton8 = findViewById(R.id.button_8);
-//        mButton9 = findViewById(R.id.button_9);
-
         mButtons.add((ImageButton) findViewById(R.id.button_1));
         mButtons.add((ImageButton) findViewById(R.id.button_2));
         mButtons.add((ImageButton) findViewById(R.id.button_3));
@@ -56,175 +36,22 @@ public class MainActivity extends AppCompatActivity {
         mButtons.add((ImageButton) findViewById(R.id.button_8));
         mButtons.add((ImageButton) findViewById(R.id.button_9));
 
-        mButton1.setTag(TicTacToeField.Figure.CROSS);
-
         mCirclePoints = findViewById(R.id.circle_points);
         mCrossPoints = findViewById(R.id.cross_points);
 
-        TicTacToeField.Figure i = TicTacToeField.Figure.CIRCLE;
-
-        mButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (mField.isEmptyCell(0, 0)) {
-                    mField.setFigure(0, 0, (TicTacToeField.Figure) mButton1.getTag());
-                    if (mButton1.getTag().equals(TicTacToeField.Figure.CROSS)) {
-                        mButton1.setTag(TicTacToeField.Figure.CIRCLE);
-                        mButton1.setImageResource(R.drawable.cross);
-                    } else if (mButton1.getTag().equals(TicTacToeField.Figure.CIRCLE)) {
-                        mButton1.setTag(TicTacToeField.Figure.CROSS);
-                        mButton1.setImageResource(R.drawable.circle);
+        for (final ImageButton button : mButtons) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int row = mButtons.indexOf(button) / 3;
+                    int column = mButtons.indexOf(button) % 3;
+                    if (mField.isEmptyCell(row, column)) {
+                        setFigure(row, column, button);
+                        checkWinner();
                     }
-
-                    checkWinner();
                 }
-            }
-        });
-
-        mButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mField.isEmptyCell(0, 1)) {
-                    mField.setFigure(0, 1, (TicTacToeField.Figure) mButton1.getTag());
-                    if (mButton1.getTag().equals(TicTacToeField.Figure.CROSS)) {
-                        mButton1.setTag(TicTacToeField.Figure.CIRCLE);
-                        mButton2.setImageResource(R.drawable.cross);
-                    } else if (mButton1.getTag().equals(TicTacToeField.Figure.CIRCLE)) {
-                        mButton1.setTag(TicTacToeField.Figure.CROSS);
-                        mButton2.setImageResource(R.drawable.circle);
-                    }
-
-                    checkWinner();
-                }
-            }
-        });
-
-        mButton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mField.isEmptyCell(0, 2)) {
-                    mField.setFigure(0, 2, (TicTacToeField.Figure) mButton1.getTag());
-                    if (mButton1.getTag().equals(TicTacToeField.Figure.CROSS)) {
-                        mButton1.setTag(TicTacToeField.Figure.CIRCLE);
-                        mButton3.setImageResource(R.drawable.cross);
-                    } else if (mButton1.getTag().equals(TicTacToeField.Figure.CIRCLE)) {
-                        mButton1.setTag(TicTacToeField.Figure.CROSS);
-                        mButton3.setImageResource(R.drawable.circle);
-                    }
-
-                    checkWinner();
-                }
-            }
-        });
-
-        mButton4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mField.isEmptyCell(1, 0)) {
-                    mField.setFigure(1, 0, (TicTacToeField.Figure) mButton1.getTag());
-                    if (mButton1.getTag().equals(TicTacToeField.Figure.CROSS)) {
-                        mButton1.setTag(TicTacToeField.Figure.CIRCLE);
-                        mButton4.setImageResource(R.drawable.cross);
-                    } else if (mButton1.getTag().equals(TicTacToeField.Figure.CIRCLE)) {
-                        mButton1.setTag(TicTacToeField.Figure.CROSS);
-                        mButton4.setImageResource(R.drawable.circle);
-                    }
-
-                    checkWinner();
-                }
-            }
-        });
-
-        mButton5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mField.isEmptyCell(1, 1)) {
-                    mField.setFigure(1, 1, (TicTacToeField.Figure) mButton1.getTag());
-                    if (mButton1.getTag().equals(TicTacToeField.Figure.CROSS)) {
-                        mButton1.setTag(TicTacToeField.Figure.CIRCLE);
-                        mButton5.setImageResource(R.drawable.cross);
-                    } else if (mButton1.getTag().equals(TicTacToeField.Figure.CIRCLE)) {
-                        mButton1.setTag(TicTacToeField.Figure.CROSS);
-                        mButton5.setImageResource(R.drawable.circle);
-                    }
-
-                    checkWinner();
-                }
-            }
-        });
-
-        mButton6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mField.isEmptyCell(1, 2)) {
-                    mField.setFigure(1, 2, (TicTacToeField.Figure) mButton1.getTag());
-                    if (mButton1.getTag().equals(TicTacToeField.Figure.CROSS)) {
-                        mButton1.setTag(TicTacToeField.Figure.CIRCLE);
-                        mButton6.setImageResource(R.drawable.cross);
-                    } else if (mButton1.getTag().equals(TicTacToeField.Figure.CIRCLE)) {
-                        mButton1.setTag(TicTacToeField.Figure.CROSS);
-                        mButton6.setImageResource(R.drawable.circle);
-                    }
-
-                    checkWinner();
-                }
-            }
-        });
-
-        mButton7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mField.isEmptyCell(2, 0)) {
-                    mField.setFigure(2, 0, (TicTacToeField.Figure) mButton1.getTag());
-                    if (mButton1.getTag().equals(TicTacToeField.Figure.CROSS)) {
-                        mButton1.setTag(TicTacToeField.Figure.CIRCLE);
-                        mButton7.setImageResource(R.drawable.cross);
-                    } else if (mButton1.getTag().equals(TicTacToeField.Figure.CIRCLE)) {
-                        mButton1.setTag(TicTacToeField.Figure.CROSS);
-                        mButton7.setImageResource(R.drawable.circle);
-                    }
-
-                    checkWinner();
-                }
-            }
-        });
-
-        mButton8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mField.isEmptyCell(2, 1)) {
-                    mField.setFigure(2, 1, (TicTacToeField.Figure) mButton1.getTag());
-                    if (mButton1.getTag().equals(TicTacToeField.Figure.CROSS)) {
-                        mButton1.setTag(TicTacToeField.Figure.CIRCLE);
-                        mButton8.setImageResource(R.drawable.cross);
-                    } else if (mButton1.getTag().equals(TicTacToeField.Figure.CIRCLE)) {
-                        mButton1.setTag(TicTacToeField.Figure.CROSS);
-                        mButton8.setImageResource(R.drawable.circle);
-                    }
-
-                    checkWinner();
-                }
-            }
-        });
-
-        mButton9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mField.isEmptyCell(2, 2)) {
-                    mField.setFigure(2, 2, (TicTacToeField.Figure) mButton1.getTag());
-                    if (mButton1.getTag().equals(TicTacToeField.Figure.CROSS)) {
-                        mButton1.setTag(TicTacToeField.Figure.CIRCLE);
-                        mButton9.setImageResource(R.drawable.cross);
-                    } else if (mButton1.getTag().equals(TicTacToeField.Figure.CIRCLE)) {
-                        mButton1.setTag(TicTacToeField.Figure.CROSS);
-                        mButton9.setImageResource(R.drawable.circle);
-                    }
-
-                    checkWinner();
-                }
-            }
-        });
+            });
+        }
     }
 
     private void checkWinner() {
@@ -232,38 +59,40 @@ public class MainActivity extends AppCompatActivity {
             case CIRCLE:
                 int points = Integer.parseInt(mCirclePoints.getText().toString());
                 mCirclePoints.setText(String.valueOf(points + 1));
-                mField = new TicTacToeField(3);
-                clearField();
+                startNewGame();
                 break;
             case CROSS:
                 points = Integer.parseInt(mCrossPoints.getText().toString());
                 mCrossPoints.setText(String.valueOf(points + 1));
-                mField = new TicTacToeField(3);
-                clearField();
+                startNewGame();
+                break;
+            case NONE:
+                if (mField.isFull()) {
+                    startNewGame();
+                }
                 break;
         }
     }
 
-    private void changeFigure() {
+    private void setFigure(int row, int column, ImageButton button) {
+        mField.setFigure(row, column, mFigure);
         switch (mFigure) {
             case CIRCLE:
+                button.setImageResource(R.drawable.circle);
                 mFigure = TicTacToeField.Figure.CROSS;
                 break;
             case CROSS:
+                button.setImageResource(R.drawable.cross);
                 mFigure = TicTacToeField.Figure.CIRCLE;
                 break;
         }
     }
 
-    private void clearField() {
-        mButton1.setImageResource(android.R.color.transparent);
-        mButton2.setImageResource(android.R.color.transparent);
-        mButton3.setImageResource(android.R.color.transparent);
-        mButton4.setImageResource(android.R.color.transparent);
-        mButton5.setImageResource(android.R.color.transparent);
-        mButton6.setImageResource(android.R.color.transparent);
-        mButton7.setImageResource(android.R.color.transparent);
-        mButton8.setImageResource(android.R.color.transparent);
-        mButton9.setImageResource(android.R.color.transparent);
+    private void startNewGame() {
+        mField = new TicTacToeField(3);
+        mFigure = TicTacToeField.Figure.CROSS;
+        for (ImageButton button : mButtons) {
+            button.setImageResource(android.R.color.transparent);
+        }
     }
 }
